2ea2e7b4ce62c26dec551b438c2cb9bf4afcf294
Esben Wiberg <ewi@projectum.com>
2021-12-11 21:53:07 +0100

    dqdwq

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # Introduction 
-TODO: Give a short introduction of your project. Let this section explain the objectives or the motivation behind this project. 
+# The Epic Formbuilder
+
+The **formbuilder** is, as the name states, a builder of forms :)
+It can **relieve** you from one of the more tiresome task of building forms thoughout your application.
+It is **highly customizable** and can be tailored to your exact needs. As crazy as they may be.
+Go through the supplied samples, to get inspired :)  
+
+As for now it's a little hardwired to use FluentUI, but the plan is to extract that part to a different package
+This will open up for people creating their own builders (it's simple AF) ie. a MaterialUI builder or any other you prefer
+In the long run I might open up to other frameworks than React
 
 # Getting Started
-TODO: Guide users through getting your code up and running on their own system. In this section you can talk about:
+It easy to get started :)  
+
 1.	Installation process
+
 2.	Software dependencies
 3.	Latest releases
 4.	API references
---
+++ cz-config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    types: [
+        {
+            value: "feat",
+            name: "✨ feat:\tAdding a new feature",
+        },
+        {
+            value: "fix",
+            name: "🐛 fix:\tFixing a bug",
+        },
+        {
+            value: "docs",
+            name: "📝 docs:\tAdd or update documentation",
+        },
+        {
+            value: "refactor",
+            name: "♻️  refactor:\tCode change that neither fixes a bug nor adds a feature",
+        },
+        {
+            value: "perf",
+            name: "⚡️ perf:\tCode change that improves performance",
+        },
+        {
+            value: "test",
+            name: "✅ test:\tAdding tests cases",
+        },
+        {
+            value: "chore",
+            name: "🚚 chore:\tChanges to the build process, pipeline or auxiliary tools\n\t\tand libraries such as documentation generation",
+        },
+        {
+            value: "wip",
+            name: "🚧 wip:\tWork in progress",
+        },
+    ],
+
+    scopes: [{ name: "formbuilder" }, { name: "docs" }, { name: "misc" }],
+
+    allowCustomScopes: true,
+    allowBreakingChanges: ["feat", "fix", "perf", "refactor"],
+
+    // askForBreakingChangeFirst: true,
+    // skip any questions you want
+    // skipQuestions: ['body'],
+    // subjectLimit: 100,
+};
